4c6f71832e8317305705d13fbc7916115bf83eb5
Basel Shbita <basel921@gmail.com>
2018-02-15 23:39:35 +0200

    Added moop-manager to list of roles - as requeted by art dep

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108193521) do
+ActiveRecord::Schema.define(version: 20180215211947) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     :index=>{:name=>"index_active_admin_comments_on_namespace"}
