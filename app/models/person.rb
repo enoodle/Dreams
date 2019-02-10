b77994ac1f25eeb5229fc2b7c2f9d0e55d35d776
Erez Freiberger <enoodle@gmail.com>
2019-02-10 02:31:32 +0200

    remove early arrival and has ticket from people csv

--- app/models/person.rb
+++ app/models/person.rb
@@ -37,14 +37,12 @@ class Person < ActiveRecord::Base
   scope :in_camp, -> { joins(:camp) }
 
   def self.to_csv
-    humanize = lambda { |x| x ? "Yes" : "No" }
     CSV.generate(headers: true) do |csv|
       csv << CSV_ATTRIBUTES.map{ |attr| attr.titleize}
       Person.includes(:camp, :roles).where(:camps => {:event_id => Rails.application.config.default_event}).each do |p|
         csv << [
           p.email, p.name, p.phone_number, p.roles.pluck(:identifier).join("|"),
           p.camp.en_name, p.camp.id,
-          humanize.call(p.needs_early_arrival), humanize.call(p.has_ticket)
         ]
       end
     end
